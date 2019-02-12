5f0c42a6d146d53aa4694cedde53e935f162c5dd
rabeek <rabeek@agastha.com>
2019-02-12 05:54:53 +0530

    VMS changes

---
+++ SkyTrack/src/main/java/com/websystique/springmvc/model/Address.java
@@ -0,0 +1,138 @@
+package com.websystique.springmvc.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ADDRESS")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int addressid;
+
+	@Column(name = "STREET1")
+	private String street1;
+
+	@Column(name = "STREET2")
+	private String street2;
+
+	@Column(name = "CITY")
+	private String city;
+
+	@Column(name = "STATE")
+	private String state;
+
+	@Column(name = "PINCODE")
+	private String pinCode;
+
+	public Integer getAddressid() {
+		return addressid;
+	}
+
+	public void setAddressid(Integer addressid) {
+		this.addressid = addressid;
+	}
+
+	public String getStreet1() {
+		return street1;
+	}
+
+	public void setStreet1(String street1) {
+		this.street1 = street1;
+	}
+
+	public String getStreet2() {
+		return street2;
+	}
+
+	public void setStreet2(String street2) {
+		this.street2 = street2;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + addressid;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((street1 == null) ? 0 : street1.hashCode());
+		result = prime * result + ((street2 == null) ? 0 : street2.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Address other = (Address) obj;
+		if (addressid != other.addressid)
+			return false;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (street1 == null) {
+			if (other.street1 != null)
+				return false;
+		} else if (!street1.equals(other.street1))
+			return false;
+		if (street2 == null) {
+			if (other.street2 != null)
+				return false;
+		} else if (!street2.equals(other.street2))
+			return false;
+		if (pinCode == null) {
+			if (other.pinCode != null)
+				return false;
+		} else if (!pinCode.equals(other.pinCode))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [addressid=" + addressid + ", street1=" + street1 + ", street2=" + street2 + ", city=" + city
+				+ ", state=" + state + ", pinCode=" + pinCode + "]";
+	}
+
+	public String getPinCode() {
+		return pinCode;
+	}
+
+	public void setPinCode(String pinCode) {
+		this.pinCode = pinCode;
+	}
+
+}
