8d94edaf8c5b8854aaaa46ae4432f54cf9ab7bbf
rabeek <rabeek@agastha.com>
2019-05-15 05:48:00 +0530

    TRIP Ajax changes

--- VehicleMonitorSystem/src/main/java/com/websystique/springmvc/controller/SendSMSController.java
+++ VehicleMonitorSystem/src/main/java/com/websystique/springmvc/controller/SendSMSController.java
@@ -1,6 +1,5 @@
 package com.websystique.springmvc.controller;
 
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
@@ -27,40 +26,33 @@ public class SendSMSController {
 	public AjaxResponseBody getSearchResultViaAjax(@RequestBody Member search) {
 
 		AjaxResponseBody result = new AjaxResponseBody();
-		
-		      String recipient = "91"+search.getDrivermobile();
-		      String message = "Your trip is arriving on the way. Driver: "+search.getDrivername()+" Mobile:"+search.getDrivermobile();
-		      String username = "gogetterrafiq";
-		      String password = "65816531";
-		      String originator = "UPDATE";
 
-		      String requestUrl;
-			try {
-				requestUrl = "https://www.txtguru.in/imobile/api.php?username=gogetterrafiq&password=65816531&source=UPDATE&dmobile=917358659978&message=TEST+SMS+FROM+TextGuru+SMS+GATEWAY" +
-				        "username=" + URLEncoder.encode(username, "UTF-8") +
-				        "&password=" + URLEncoder.encode(password, "UTF-8") +
-				        "&recipient=" + URLEncoder.encode(recipient, "UTF-8") +
-				        "&messagetype=SMS:TEXT" +
-				        "&messagedata=" + URLEncoder.encode(message, "UTF-8") +
-				        "&originator=" + URLEncoder.encode(originator, "UTF-8") +
-				        "&serviceprovider=GSMModem1" +
-				        "&responseformat=html";
-			
+		String recipient = "91" + search.getDrivermobile();
+		String message = "Your+trip+is+arriving+on+the+way.+Driver:+" + search.getDrivername() + "+Mobile:+"
+				+ search.getDrivermobile();
+		String username = "gogetterrafiq";
+		String password = "65816531";
+		String originator = "SKYCAB";
 
+		String requestUrl;
+		try {
+			requestUrl = "https://www.txtguru.in/imobile/api.php?username=" + username + "&password=" + password
+					+ "&source=" + originator + "&dmobile=" + recipient + "&message=" + message + "&username="
+					+ URLEncoder.encode(username, "UTF-8") + "&password=" + URLEncoder.encode(password, "UTF-8")
+					+ "&recipient=" + URLEncoder.encode(recipient, "UTF-8") + "&messagetype=SMS:TEXT" + "&messagedata="
+					+ URLEncoder.encode(message, "UTF-8") + "&originator=" + URLEncoder.encode(originator, "UTF-8")
+					+ "&serviceprovider=GSMModem1" + "&responseformat=html";
 
-		      URL url = new URL(requestUrl);
-		      HttpURLConnection uc = (HttpURLConnection)url.openConnection();
-		      System.out.println(uc.getResponseCode());
-		      System.out.println(uc.getResponseMessage());
+			URL url = new URL(requestUrl);
+			HttpURLConnection uc = (HttpURLConnection) url.openConnection();
+			System.out.println(uc.getResponseCode());
+			System.out.println(uc.getResponseMessage());
 
-		      uc.disconnect();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-
-		   
-
+			uc.disconnect();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 		result.setCode("400");
 		result.setMsg("Search criteria is empty!");
